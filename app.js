5a1e542ec7921ae406b1d16d9dc41785cc59b621
Nehal Ali <nehalalikamran103@gmail.com>
2025-10-27 02:25:18 -0700

    JS assignment chp-2

---
+++ app.js
@@ -0,0 +1,30 @@
+function hello() {
+    alert("Hello world");
+}
+
+function myName() {
+    alert("Name: Nehal ALi");
+    alert("Age: 18");
+    alert("Deploam: Smit Moblie Application Development Learning . . .");
+}
+
+function pizza() {
+    alert("Pizza");
+}
+
+function gmail()
+
+{
+    alert ("My Gmail ID is nihalalikamran@gmail.com");
+}
+
+function book()
+{
+    alert ("I Am trying to learn from the book A smarter wray to learn JavaScript");
+
+}
+
+function show () 
+{
+    alert ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+}
